fd17622d95451bf5398499b5f1cf5c559bff35d3
lendoo <lendoo73@gmail.com>
2020-09-25 16:26:20 +0100

    Update script.py

--- python/Analyze_Financial_Data_with_Python/Calculating_Financial_Statistics/script.py
+++ python/Analyze_Financial_Data_with_Python/Calculating_Financial_Statistics/script.py
@@ -1,2 +1,10 @@
+from math import log
+
 def display_as_percentage(val):
   return "{:.1f}%".format(val * 100)
+
+def calculate_simple_return(start_price, end_price, dividend = 0):
+  return (end_price - start_price + dividend) / start_price
+
+def calculate_log_return(start_price, end_price):
+  return log(end_price / start_price)
