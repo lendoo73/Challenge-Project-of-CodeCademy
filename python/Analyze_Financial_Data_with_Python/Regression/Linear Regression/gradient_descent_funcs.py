d6c14c4abe34e2ed5d26b9927d0a30f308cf4fd7
lendoo <lendoo73@gmail.com>
2020-10-02 18:28:24 +0100

    Create gradient_descent_funcs.py

---
+++ python/Analyze_Financial_Data_with_Python/Regression/Linear Regression/gradient_descent_funcs.py
@@ -0,0 +1,34 @@
+def get_gradient_at_b(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+    x_val = x[i]
+    y_val = y[i]
+    diff += (y_val - ((m * x_val) + b))
+  b_gradient = -(2/N) * diff  
+  return b_gradient
+
+def get_gradient_at_m(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+      x_val = x[i]
+      y_val = y[i]
+      diff += x_val * (y_val - ((m * x_val) + b))
+  m_gradient = -(2/N) * diff  
+  return m_gradient
+
+#Your step_gradient function here
+def step_gradient(b_current, m_current, x, y, learning_rate):
+    b_gradient = get_gradient_at_b(x, y, b_current, m_current)
+    m_gradient = get_gradient_at_m(x, y, b_current, m_current)
+    b = b_current - (learning_rate * b_gradient)
+    m = m_current - (learning_rate * m_gradient)
+    return [b, m]
+
+def gradient_descent(x, y, learning_rate, num_iterations):
+    b = 0
+    m = 0
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, x, y, learning_rate)
+    return [b, m]
