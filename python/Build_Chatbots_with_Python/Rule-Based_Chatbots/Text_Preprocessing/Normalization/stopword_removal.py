2d494bc7f8b536565d2b68e5f968fb2ad8e34de7
lendoo <lendoo73@gmail.com>
2020-10-16 16:21:41 +0100

    Create stopword_removal.py

---
+++ python/Build_Chatbots_with_Python/Rule-Based_Chatbots/Text_Preprocessing/Normalization/stopword_removal.py
@@ -0,0 +1,27 @@
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+survey_text = 'A YouGov study found that American\'s like Italian food more than any other country\'s cuisine.'
+
+#  Save all English stopwords, as a set:
+stop_words = set(stopwords.words("english"))
+
+# Tokenize the text:
+tokenized_survey = word_tokenize(survey_text)
+
+# Remove stop words:
+text_no_stops = [word for word in tokenized_survey if word not in stop_words]
+
+try:
+  print(f'Stopwords type: {type(stop_words)}')
+except:
+  print('Expected a variable called `stop_words`')
+try:
+  print(f'Words Tokenized: {tokenized_survey}')
+except:
+  print('Expected a variable called `tokenized_survey`')
+try:
+  print(f'Text without Stops: {text_no_stops}')
+except:
+  print('Expected a variable called `text_no_stops`')
+  
