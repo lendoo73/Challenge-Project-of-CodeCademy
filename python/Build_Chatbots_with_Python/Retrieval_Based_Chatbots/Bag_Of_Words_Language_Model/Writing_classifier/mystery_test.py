db119adf18640772548fabefbb3a173ae92b140b
lendoo <lendoo73@gmail.com>
2020-10-19 17:45:24 +0100

    Create mystery_test.py

---
+++ python/Build_Chatbots_with_Python/Retrieval_Based_Chatbots/Bag_Of_Words_Language_Model/Writing_classifier/mystery_test.py
@@ -0,0 +1,20 @@
+postcard_test = """
+My friend,
+From the 10th of July to the 13th, a fierce storm raged, clouds of
+freeing spray broke over the ship, incasing her in a coat of icy mail,
+and the tempest forced all of the ice out of the lower end of the
+channel and beyond as far as the eye could see, but the _Roosevelt_
+still remained surrounded by ice.
+Hope to see you soon.
+"""
+
+# an email:
+email_test = """
+Hi CSABA,
+
+We’ve been notified that a parcel has been posted to you from SALUTICA ALLIED SOLUTIONS SDN BHD in Malaysia.
+
+Just to let you know, it’s likely there will be a Customs charge to pay for this item, due to the declared value of it.
+
+You don’t need to do anything now. We’ll let you know when it’s in the UK and confirm if any charges have been applied by Border Force.
+"""
