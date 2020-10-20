9a6db2d697162fab237b2ff8c0c6b93a36c95d0a
lendoo <lendoo73@gmail.com>
2020-10-20 10:40:08 +0100

    Create script.py

---
+++ python/Build_Chatbots_with_Python/Retrieval_Based_Chatbots/Term_frequency_inverse_document_frequency/Inverse_document_frequency/script.py
@@ -0,0 +1,22 @@
+import codecademylib3_seaborn
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfTransformer
+from term_frequency import term_frequencies, feature_names, df_term_frequencies
+
+# display term-document matrix of term frequencies
+print(df_term_frequencies)
+
+# initialize and fit TfidfTransformer
+transformer = TfidfTransformer()
+
+transformer.fit(term_frequencies)
+
+# stores the inverse document frequencies of the terms as a NumPy array:
+idf_values = transformer.idf_
+
+# create pandas DataFrame with inverse document frequencies
+try:
+  df_idf = pd.DataFrame(idf_values, index = feature_names, columns=['Inverse Document Frequency'])
+  print(df_idf)
+except:
+  pass
