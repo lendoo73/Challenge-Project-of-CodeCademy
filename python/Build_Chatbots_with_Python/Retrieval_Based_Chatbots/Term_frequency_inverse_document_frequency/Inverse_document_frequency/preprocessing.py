c4e32940930d363292081c7b062464ed155a2484
lendoo <lendoo73@gmail.com>
2020-10-20 10:42:07 +0100

    Create preprocessing.py

---
+++ python/Build_Chatbots_with_Python/Retrieval_Based_Chatbots/Term_frequency_inverse_document_frequency/Inverse_document_frequency/preprocessing.py
@@ -0,0 +1,25 @@
+import nltk, re
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+
+stop_words = stopwords.words('english')
+normalizer = WordNetLemmatizer()
+
+def get_part_of_speech(word):
+  probable_part_of_speech = wordnet.synsets(word)
+  pos_counts = Counter()
+  pos_counts["n"] = len(  [ item for item in probable_part_of_speech if item.pos()=="n"]  )
+  pos_counts["v"] = len(  [ item for item in probable_part_of_speech if item.pos()=="v"]  )
+  pos_counts["a"] = len(  [ item for item in probable_part_of_speech if item.pos()=="a"]  )
+  pos_counts["r"] = len(  [ item for item in probable_part_of_speech if item.pos()=="r"]  )
+  most_likely_part_of_speech = pos_counts.most_common(1)[0][0]
+  return most_likely_part_of_speech
+
+def preprocess_text(text):
+  cleaned = re.sub(r'\W+', ' ', text).lower()
+  tokenized = word_tokenize(cleaned)
+  normalized = " ".join([normalizer.lemmatize(token, get_part_of_speech(token)) for token in tokenized])
+  return normalized
