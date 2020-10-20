e47fe273536e50fe852d061373d311f3bff57602
lendoo <lendoo73@gmail.com>
2020-10-20 12:09:55 +0100

    Create poems.py

---
+++ python/Build_Chatbots_with_Python/Retrieval_Based_Chatbots/Term_frequency_inverse_document_frequency/Converting_Bag_of_Words_to_Tf_idf/poems.py
@@ -0,0 +1,87 @@
+poem_1 = '''
+Success is counted sweetest
+By those who ne'er succeed.
+To comprehend a nectar
+Requires sorest need.
+
+Not one of all the purple host
+Who took the flag to-day
+Can tell the definition,
+So clear, of victory,
+
+As he, defeated, dying,
+On whose forbidden ear
+The distant strains of triumph
+Break, agonized and clear!'''
+
+poem_2 = '''
+Wild nights! Wild nights!
+Were I with thee,
+Wild nights should be
+Our luxury!
+
+Futile the winds
+To a heart in port, —
+Done with the compass,
+Done with the chart.
+
+Rowing in Eden!
+Ah! the sea!
+Might I but moor
+To-night in thee!'''
+
+poem_3 = '''
+I'm nobody! Who are you?
+Are you nobody, too?
+Then there 's a pair of us — don't tell!
+They 'd banish us, you know.
+
+How dreary to be somebody!
+How public, like a frog
+To tell your name the livelong day
+To an admiring bog!'''
+
+poem_4 = '''
+I felt a funeral in my brain,
+   And mourners, to and fro,
+Kept treading, treading, till it seemed
+   That sense was breaking through.
+
+And when they all were seated,
+   A service like a drum
+Kept beating, beating, till I thought
+   My mind was going numb.
+
+And then I heard them lift a box,
+   And creak across my soul
+With those same boots of lead, again.
+   Then space began to toll
+
+As all the heavens were a bell,
+   And Being but an ear,
+And I and silence some strange race,
+   Wrecked, solitary, here.'''
+
+poem_5 = '''
+Hope is the thing with feathers
+That perches in the soul,
+And sings the tune without the words,
+And never stops at all,
+
+And sweetest in the gale is heard;
+And sore must be the storm
+That could abash the little bird
+That kept so many warm.
+
+I 've heard it in the chillest land,
+And on the strangest sea;
+Yet, never, in extremity,
+It asked a crumb of me.'''
+
+poem_6 = '''
+The pedigree of honey
+Does not concern the bee;
+A clover, any time, to him
+Is aristocracy.'''
+
+poems = [poem_1, poem_2, poem_3, poem_4, poem_5, poem_6]
