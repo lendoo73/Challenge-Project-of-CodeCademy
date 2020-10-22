c6a04199b62476e41c05bf593e2c22a91e36a614
lendoo <lendoo73@gmail.com>
2020-10-22 15:20:52 +0100

    Create script.py

---
+++ python/Build_Chatbots_with_Python/Retrieval_Based_Chatbots/Word_Embeddings/Distance/script.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy.spatial.distance import cityblock, euclidean, cosine
+import spacy
+
+# load word embedding model
+nlp = spacy.load('en')
+
+# define word embedding vectors
+happy_vec = nlp('happy').vector
+sad_vec = nlp('sad').vector
+angry_vec = nlp('angry').vector
+
+# calculate Manhattan distance
+man_happy_sad = cityblock(happy_vec, sad_vec)
+man_sad_angry = cityblock(sad_vec, angry_vec)
+
+print(man_happy_sad)
+print(man_sad_angry)
+
+# calculate Euclidean distance
+euc_happy_sad = euclidean(happy_vec, sad_vec)
+euc_sad_angry = euclidean(sad_vec, angry_vec)
+
+print(euc_happy_sad)
+print(euc_sad_angry)
+
+# calculate cosine distance
+cos_happy_sad = cosine(happy_vec, sad_vec)
+cos_sad_angry = cosine(sad_vec, angry_vec)
+
+print(cos_happy_sad)
+print(cos_sad_angry)
+
+
