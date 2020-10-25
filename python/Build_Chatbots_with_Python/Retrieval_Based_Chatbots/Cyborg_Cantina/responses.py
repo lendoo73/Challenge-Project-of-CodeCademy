692b7dfc04aa3ae3da0836bec3e47596fd273308
lendoo <lendoo73@gmail.com>
2020-10-25 11:58:06 +0000

    Create responses.py

---
+++ python/Build_Chatbots_with_Python/Retrieval_Based_Chatbots/Cyborg_Cantina/responses.py
@@ -0,0 +1,7 @@
+response_a = "The {} has a gluten-free option, but it is not vegan"
+response_b = "We have a selection of sides to go along with the {}, including mashed potatoes and steamed vegatables."
+response_c = "{} includes habanero, so it is a bit spicy!"
+blank_spot = "food"
+
+
+responses = [response_a, response_b, response_c]
