c7eac5bc5cd9bb6ef6f3c588476c4e0a9c95b412
lendoo <lendoo73@gmail.com>
2020-10-25 11:57:40 +0000

    Create user_functions.py

---
+++ python/Build_Chatbots_with_Python/Retrieval_Based_Chatbots/Cyborg_Cantina/user_functions.py
@@ -0,0 +1,36 @@
+import re
+from collections import Counter
+import spacy
+word2vec = spacy.load('en')
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words("english"))
+
+def preprocess(input_sentence):
+    input_sentence = input_sentence.lower()
+    input_sentence = re.sub(r'[^\w\s]','',input_sentence)
+    tokens = word_tokenize(input_sentence)
+    input_sentence = [i for i in tokens if not i in stop_words]
+    return(input_sentence)
+
+def compare_overlap(user_message, possible_response):
+    similar_words = 0
+    for token in user_message:
+        if token in possible_response:
+              similar_words += 1
+    return similar_words
+  
+def extract_nouns(tagged_message):
+    message_nouns = list()
+    for token in tagged_message:
+        if token[1].startswith("N"):
+            message_nouns.append(token[0])
+    return message_nouns
+
+def compute_similarity(tokens, category):
+    output_list = list()
+    for token in tokens:
+        output_list.append([token.text, category.text, token.similarity(category)])
+    return output_list
+  
