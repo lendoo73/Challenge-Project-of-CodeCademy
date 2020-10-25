fd0bee5d61ecb5f9c67ebfde19d07373c37848f5
lendoo <lendoo73@gmail.com>
2020-10-25 10:16:08 +0000

    Create data.py

---
+++ python/Build_Chatbots_with_Python/Retrieval_Based_Chatbots/Building_a_Retrieval_System/data.py
@@ -0,0 +1,5 @@
+blank_spot = "illinois"
+response_a = "The average temperature this weekend in {} will be 88 degrees. Bring your sunglasses!"
+response_b = "Forget about your umbrella; there is no rain forecasted in {} this weekend."
+response_c = "This weekend, a warm front from the southeast will keep skies near {} clear."
+responses= [response_a, response_b, response_c]
