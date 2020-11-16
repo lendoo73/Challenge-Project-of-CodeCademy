731d2dacf749563457bc0ceb282344e92431fe31
lendoo <lendoo73@gmail.com>
2020-11-16 10:25:49 +0000

    Create plotting.py

---
+++ python/Build_Deep_Learning_Models_with_TensorFlow/Hyperparameter_Tuning/Regularization_dropout/plotting.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+
+def plot(history, path):
+  #plotting
+  fig, axs = plt.subplots(1, 2, gridspec_kw={'hspace': 1, 'wspace': 0.5}) 
+  (ax1, ax2) = axs
+  ax1.plot(history.history['loss'], label='train')
+  ax1.plot(history.history['val_loss'], label='validation')
+  ax1.legend(loc="upper right")
+  ax1.set_xlabel("# of epochs")
+  ax1.set_ylabel("loss (mse)")
+
+  ax2.plot(history.history['mae'], label='train')
+  ax2.plot(history.history['val_mae'], label='validation')
+  ax2.legend(loc="upper right")
+  ax2.set_xlabel("# of epochs")
+  ax2.set_ylabel("MAE")
+
+  plt.savefig(path)
+
+
+  
