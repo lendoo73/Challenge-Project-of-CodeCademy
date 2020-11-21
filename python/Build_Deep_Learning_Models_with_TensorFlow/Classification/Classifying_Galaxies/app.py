bd965622754b0fa0cd351e368e200f8dc39775e3
lendoo <lendoo73@gmail.com>
2020-11-21 13:39:52 +0000

    Update app.py

--- python/Build_Deep_Learning_Models_with_TensorFlow/Classification/Classifying_Galaxies/app.py
+++ python/Build_Deep_Learning_Models_with_TensorFlow/Classification/Classifying_Galaxies/app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
