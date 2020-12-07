0d1cfc8445f86ee64f824b88c4f59225e3f8a6d0
lendoo <lendoo73@gmail.com>
2020-12-07 12:55:11 +0000

    Update script.py

--- python/Learn_the_Basics_of_Machine_Learning/Distance_formula/script.py
+++ python/Learn_the_Basics_of_Machine_Learning/Distance_formula/script.py
@@ -15,12 +15,38 @@ def euclidean_distance(pt1, pt2):
   return distance ** 0.5
 
 def manhattan_distance(pt1, pt2):
+  if not equal_dimension(pt1, pt2):
+    return "ERROR: Different number of dimensions!"
+  
   distance = 0
   for i in range(len(pt1)):
-    distance += abs(pt1[i] - pt2[i])
+    difference = abs(pt1[i] - pt2[i])
+    distance += difference
   return distance
 
-print("Euclidean  distance:")
+def hamming_distance(pt1, pt2):
+  if not equal_dimension(pt1, pt2):
+    return "ERROR: Different number of dimensions!"
+  
+  distance = 0
+  for i in range(len(pt1)):
+    difference = 0 if pt1[i] == pt2[i] else 1
+    distance += difference
+  return distance
+
+print("Euclidean distance:")
 print(euclidean_distance([1, 2], [4, 0]))
 print(euclidean_distance([5, 4, 3], [1, 7, 9]))
 print(euclidean_distance([2, 3, 4], [1, 2]))
+
+print("Manhattan distance:")
+print(manhattan_distance([1, 2], [4, 0]))
+print(manhattan_distance([5, 4, 3], [1, 7, 9]))
+print(manhattan_distance([2, 3, 4], [1, 2]))
+
+print("Hamming distance:")
+print(hamming_distance([1, 2], [4, 0]))
+print(hamming_distance([5, 4, 3], [1, 7, 9]))
+print(hamming_distance([2, 3, 4], [1, 2]))
+print(hamming_distance([1, 2], [1, 100]))
+print(hamming_distance([5, 4, 9], [1, 7, 9]))
