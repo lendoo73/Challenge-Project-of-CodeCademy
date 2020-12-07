fa0a33e4cc59f7693d4ffc025d7de4fd072983af
lendoo <lendoo73@gmail.com>
2020-12-07 12:42:45 +0000

    Create script.py

---
+++ python/Learn_the_Basics_of_Machine_Learning/Distance_formula/script.py
@@ -0,0 +1,19 @@
+def equal_dimension(pt1, pt2):
+  if len(pt1) == len(pt2):
+    return True
+
+def euclidean_distance(pt1, pt2):
+  
+  if not equal_dimension(pt1, pt2):
+    return "ERROR: Different number of dimensions!"
+
+  distance = 0
+  for i in range(len(pt1)):
+    difference = (pt2[i] - pt1[i]) ** 2
+    distance += difference
+
+  return distance ** 0.5
+
+print(euclidean_distance([1, 2], [4, 0]))
+print(euclidean_distance([5, 4, 3], [1, 7, 9]))
+print(euclidean_distance([2, 3, 4], [1, 2]))
