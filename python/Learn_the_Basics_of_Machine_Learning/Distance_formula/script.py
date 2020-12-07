f7147d41f6e72f175c0198444e6ff730e50d7d6d
lendoo <lendoo73@gmail.com>
2020-12-07 12:45:54 +0000

    Update script.py

--- python/Learn_the_Basics_of_Machine_Learning/Distance_formula/script.py
+++ python/Learn_the_Basics_of_Machine_Learning/Distance_formula/script.py
@@ -14,6 +14,13 @@ def euclidean_distance(pt1, pt2):
 
   return distance ** 0.5
 
+def manhattan_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    distance += abs(pt1[i] - pt2[i])
+  return distance
+
+print("Euclidean  distance:")
 print(euclidean_distance([1, 2], [4, 0]))
 print(euclidean_distance([5, 4, 3], [1, 7, 9]))
 print(euclidean_distance([2, 3, 4], [1, 2]))
