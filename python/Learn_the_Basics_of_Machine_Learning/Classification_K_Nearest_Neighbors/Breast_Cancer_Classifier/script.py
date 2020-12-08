2acb18055ed645f9d7f00453969d03302bbb3702
lendoo <lendoo73@gmail.com>
2020-12-08 18:23:45 +0000

    Update script.py

--- python/Learn_the_Basics_of_Machine_Learning/Classification_K_Nearest_Neighbors/Breast_Cancer_Classifier/script.py
+++ python/Learn_the_Basics_of_Machine_Learning/Classification_K_Nearest_Neighbors/Breast_Cancer_Classifier/script.py
@@ -1,4 +1,3 @@
-import codecademylib3_seaborn
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
