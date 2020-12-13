cb7e70cb28960f0f22f08f3e336d8851c752abac
lendoo <lendoo73@gmail.com>
2020-12-13 09:53:18 +0000

    Update gini_impurity.py

--- python/Learn_the_Basics_of_Machine_Learning/Decision_Trees/gini_impurity.py
+++ python/Learn_the_Basics_of_Machine_Learning/Decision_Trees/gini_impurity.py
@@ -1,25 +1,14 @@
-from collections import Counter
-
 labels = ["unacc", "unacc", "acc", "acc", "good", "good"]
 labels = ["unacc","unacc","unacc", "good", "vgood", "vgood"]
 #labels = ["unacc", "unacc", "unacc", "unacc", "unacc", "unacc"]
-"""
-impurity = 1
 
-label_counts = Counter(labels)
-print(label_counts)
-
-for label in label_counts:
-  probability_of_label = label_counts[label] / len(labels)
-  impurity = impurity - probability_of_label ** 2
-"""
 def gini(labels):
   label_keys = set(labels)
+  total_number_of_labels = len(labels)
   impurity = 1
   for label in label_keys:
-    probability_of_label = labels.count(label) / len(labels)
+    probability_of_label = labels.count(label) / total_number_of_labels
     impurity = impurity - probability_of_label ** 2
   return impurity
 
 print(gini(labels))
-#print(impurity)
