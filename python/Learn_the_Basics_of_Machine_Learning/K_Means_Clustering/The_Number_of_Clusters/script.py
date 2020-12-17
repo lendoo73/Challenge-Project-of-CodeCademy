f7bb00b7d03462f44ae7cecc77562d793f9c5ec6
lendoo <lendoo73@gmail.com>
2020-12-17 15:45:33 +0000

    Create script.py

---
+++ python/Learn_the_Basics_of_Machine_Learning/K_Means_Clustering/The_Number_of_Clusters/script.py
@@ -0,0 +1,31 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.cluster import KMeans
+
+iris = datasets.load_iris()
+
+samples = iris.data
+
+# Code Start here:
+num_clusters = list(range(1, 9))
+inertias = []
+
+for k in num_clusters:
+  model = KMeans(n_clusters = k)
+  model.fit(samples)
+  inertias.append(model.inertia_)
+
+plt.plot(
+  num_clusters,
+  inertias,
+  "-o"
+)
+plt.xlabel('Number of Clusters (k)')
+plt.ylabel('Inertia')
+ 
+plt.show()
+
+
