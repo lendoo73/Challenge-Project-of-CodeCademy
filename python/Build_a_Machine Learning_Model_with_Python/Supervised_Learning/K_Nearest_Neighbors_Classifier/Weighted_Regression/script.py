11a16973d5a0224d73d7837e0d90aa302af668f4
lendoo <lendoo73@gmail.com>
2020-12-26 17:47:47 +0000

    Create script.py

---
+++ python/Build_a_Machine Learning_Model_with_Python/Supervised_Learning/K_Nearest_Neighbors_Classifier/Weighted_Regression/script.py
@@ -0,0 +1,44 @@
+from movies import movie_dataset, movie_ratings
+
+def distance(movie1, movie2):
+  squared_difference = 0
+  for i in range(len(movie1)):
+    squared_difference += (movie1[i] - movie2[i]) ** 2
+  final_distance = squared_difference ** 0.5
+  return final_distance
+
+def predict(unknown, dataset, movie_ratings, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #Taking only the k closest points
+  neighbors = distances[0:k]
+  
+  numerator = 0
+  denominator = 0
+
+  for neighbor in neighbors:
+    title = neighbor[1]
+    rating = movie_ratings[title]
+    distance_to_neighbor = neighbor[0]
+    
+    numerator += rating / distance_to_neighbor
+    denominator += 1 / distance_to_neighbor
+
+  weighted_average = numerator / denominator
+
+  return weighted_average
+
+Incredibles_2 = [0.016, 0.300, 1.022]
+print(predict(
+  Incredibles_2,
+  movie_dataset,
+  movie_ratings,
+  5
+))
+
