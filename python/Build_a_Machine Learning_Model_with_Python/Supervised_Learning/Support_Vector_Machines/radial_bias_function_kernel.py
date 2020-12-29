9d325f4f7a69cb9a9831e6b85235834da56e421e
lendoo <lendoo73@gmail.com>
2020-12-29 09:40:47 +0000

    Create radial_bias_function_kernel.py

---
+++ python/Build_a_Machine Learning_Model_with_Python/Supervised_Learning/Support_Vector_Machines/radial_bias_function_kernel.py
@@ -0,0 +1,16 @@
+from data import points, labels
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+training_data, validation_data, training_labels, validation_labels = train_test_split(points, labels, train_size = 0.8, test_size = 0.2, random_state = 100)
+
+classifier = SVC(
+  kernel = "rbf",
+  #gamma = 1      # accuracy: 0.88
+  #gamma = 10     # accuracy: 0.83
+  gamma = 0.1     # accuracy: 0.83
+)
+
+classifier.fit(training_data, training_labels)
+
+print(classifier.score(validation_data, validation_labels))
