c9e27597d45e638b8c5124ab9d948c67f103744c
lendoo <lendoo73@gmail.com>
2021-01-12 16:19:45 +0000

    Update app.py

--- python/Learn_Flask/Flask_Forms/app.py
+++ python/Learn_Flask/Flask_Forms/app.py
@@ -7,9 +7,9 @@ app.config["SECRET_KEY"] = "mysecret"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-  recipe_form = RecipeForm(csrf_enabled=False)
+  recipe_form = RecipeForm(csrf_enabled = False)
   if recipe_form.validate_on_submit():
-    new_id = len(recipes)+1
+    new_id = len(recipes) + 1
     recipes[new_id] = recipe_form.recipe.data
     types[new_id] = recipe_form.recipe_type.data
     descriptions[new_id] = recipe_form.description.data
@@ -18,15 +18,31 @@ def index():
     add_ingredients(new_id, new_ingredients)
     add_instructions(new_id, new_instructions)
     comments[new_id] = []
-  return render_template("index.html", template_recipes=recipes, template_form=recipe_form)
+  return render_template(
+    "index.html", 
+    template_recipes = recipes, 
+    template_form = recipe_form
+  )
 
-@app.route("/recipe/<int:id>", methods=["GET", "POST"])
+@app.route(
+  "/recipe/<int:id>", 
+  methods = ["GET", "POST"]
+)
 def recipe(id):
-  comment_form = CommentForm(csrf_enabled=False)
+  comment_form = CommentForm(csrf_enabled = False)
   if comment_form.validate_on_submit():
     new_comment = comment_form.comment.data
     comments[id].append(new_comment)
-  return render_template("recipe.html", template_recipe=recipes[id], template_type=types[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
+  return render_template(
+    "recipe.html", 
+    template_recipe = recipes[id], 
+    template_type = types[id], 
+    template_description = descriptions[id], 
+    template_ingredients = ingredients[id], 
+    template_instructions = instructions[id], 
+    template_comments = comments[id], 
+    template_form = comment_form
+  )
 
 @app.route("/about")
 def about():
