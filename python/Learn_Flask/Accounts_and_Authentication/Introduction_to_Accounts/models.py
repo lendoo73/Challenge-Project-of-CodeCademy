bcfb4f8aa599779a651321ae4392c4c8828c60db
lendoo <lendoo73@gmail.com>
2021-01-25 11:38:46 +0000

    Create models.py

---
+++ python/Learn_Flask/Accounts_and_Authentication/Introduction_to_Accounts/models.py
@@ -0,0 +1,31 @@
+from app import db, login_manager
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+# User model
+class User(UserMixin, db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), index=True, unique=True)
+  email = db.Column(db.String(120), unique = True, index = True)
+  password_hash = db.Column(db.String(128))
+  joined_at = db.Column(db.DateTime(), default = datetime.utcnow, index = True)
+
+  def __repr__(self):
+    return '<User {}>'.format(self.username)
+
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+# DinnerParty model
+class DinnerParty(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date = db.Column(db.String(140))
+  venue = db.Column(db.String(140))
+  main_dish = db.Column(db.String(140))
+  number_seats = db.Column(db.Integer)
+  party_host_id = db.Column(db.Integer)
+  attendees = db.Column(db.String(256))
