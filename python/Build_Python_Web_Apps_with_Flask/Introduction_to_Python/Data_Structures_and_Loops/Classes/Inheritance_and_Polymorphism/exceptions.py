ac6d6ddccfdbcee12954fae4d018693d608e03c6
lendoo <lendoo73@gmail.com>
2021-01-31 14:24:43 +0000

    Create exceptions.py

---
+++ python/Build_Python_Web_Apps_with_Flask/Introduction_to_Python/Data_Structures_and_Loops/Classes/Inheritance_and_Polymorphism/exceptions.py
@@ -0,0 +1,20 @@
+# Define your exception up here:
+class OutOfStock(Exception):
+  pass
+
+# Update the class below to raise OutOfStock
+class CandleShop:
+  name = "Here's a Hot Tip: Buy Drip Candles"
+  def __init__(self, stock):
+    self.stock = stock
+    
+  def buy(self, color):
+    if self.stock[color] < 1:
+      raise OutOfStock
+    self.stock[color] = self.stock[color] - 1
+
+candle_shop = CandleShop({'blue': 6, 'red': 2, 'green': 0})
+candle_shop.buy('blue')
+
+# This should raise OutOfStock:
+candle_shop.buy('green')
