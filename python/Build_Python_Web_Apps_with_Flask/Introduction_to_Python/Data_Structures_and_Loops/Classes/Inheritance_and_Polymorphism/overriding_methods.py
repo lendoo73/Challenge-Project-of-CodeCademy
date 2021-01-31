93b9fcdc130a708a21088ca3af4d7ec345d969ed
lendoo <lendoo73@gmail.com>
2021-01-31 14:35:01 +0000

    Create overriding_methods.py

---
+++ python/Build_Python_Web_Apps_with_Flask/Introduction_to_Python/Data_Structures_and_Loops/Classes/Inheritance_and_Polymorphism/overriding_methods.py
@@ -0,0 +1,17 @@
+class Message:
+  def __init__(self, sender, recipient, text):
+    self.sender = sender
+    self.recipient = recipient
+    self.text = text
+
+class User:
+  def __init__(self, username):
+    self.username = username
+    
+  def edit_message(self, message, new_text):
+    if message.sender == self.username:
+      message.text = new_text
+
+class Admin(User):
+  def edit_message(self, message, new_text):
+    message.text = new_text
