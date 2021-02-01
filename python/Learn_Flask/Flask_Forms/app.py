c003d3075f681df509f30755fdd8d3c312fe752c
lendoo <lendoo73@gmail.com>
2021-02-01 18:51:36 +0000

    Update app.py

--- python/Learn_Flask/Flask_Forms/app.py
+++ python/Learn_Flask/Flask_Forms/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect, url_for
 from helper import recipes, types, descriptions, ingredients, instructions, add_ingredients, add_instructions, comments
 from forms import RecipeForm, CommentForm
 
@@ -7,9 +7,9 @@ app.config["SECRET_KEY"] = "mysecret"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-  recipe_form = RecipeForm(csrf_enabled = False)
+  recipe_form = RecipeForm(csrf_enabled=False)
   if recipe_form.validate_on_submit():
-    new_id = len(recipes) + 1
+    new_id = len(recipes)+1
     recipes[new_id] = recipe_form.recipe.data
     types[new_id] = recipe_form.recipe_type.data
     descriptions[new_id] = recipe_form.description.data
@@ -18,31 +18,18 @@ def index():
     add_ingredients(new_id, new_ingredients)
     add_instructions(new_id, new_instructions)
     comments[new_id] = []
-  return render_template(
-    "index.html", 
-    template_recipes = recipes, 
-    template_form = recipe_form
-  )
+    # redirect to the newly created recipe
+    return redirect(url_for("recipe", id = new_id, _external = True, _scheme = "https"))
 
-@app.route(
-  "/recipe/<int:id>", 
-  methods = ["GET", "POST"]
-)
+  return render_template("index.html", template_recipes=recipes, template_form=recipe_form)
+
+@app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
-  comment_form = CommentForm(csrf_enabled = False)
+  comment_form = CommentForm(csrf_enabled=False)
   if comment_form.validate_on_submit():
     new_comment = comment_form.comment.data
     comments[id].append(new_comment)
-  return render_template(
-    "recipe.html", 
-    template_recipe = recipes[id], 
-    template_type = types[id], 
-    template_description = descriptions[id], 
-    template_ingredients = ingredients[id], 
-    template_instructions = instructions[id], 
-    template_comments = comments[id], 
-    template_form = comment_form
-  )
+  return render_template("recipe.html", template_recipe=recipes[id], template_type=types[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
 
 @app.route("/about")
 def about():
