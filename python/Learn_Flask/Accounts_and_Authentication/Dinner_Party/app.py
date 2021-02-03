b0c3a50dd41baf30ac63f2f693eff808d3cac67f
lendoo <lendoo73@gmail.com>
2021-02-03 19:48:18 +0000

    Create app.py

---
+++ python/Learn_Flask/Accounts_and_Authentication/Dinner_Party/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# instantiate application and database
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# create login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+import routes
+import models
