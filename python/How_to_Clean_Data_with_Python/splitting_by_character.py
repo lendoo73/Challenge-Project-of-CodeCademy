35ea44ee79ef8531ef207a2336a13c7200c9a98c
lendoo <lendoo73@gmail.com>
2021-03-27 23:08:31 +0000

    Create splitting_by_character.py

---
+++ python/How_to_Clean_Data_with_Python/splitting_by_character.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from students import students
+
+
+name_split = students.full_name.str.split(" ")
+students["first_name"] = name_split.str.get(0)
+students["last_name"] = name_split.str.get(1)
+
+print(students.head())
+
+
