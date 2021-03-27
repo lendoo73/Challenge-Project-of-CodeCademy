4dee5bb5d17943c9e69331ec97373435931d1ecb
lendoo <lendoo73@gmail.com>
2021-03-27 22:43:03 +0000

    Create reshaping.py

---
+++ python/How_to_Clean_Data_with_Python/reshaping.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from students import students
+
+print(students.columns)
+students = pd.melt(
+  frame = students, 
+  id_vars = ['full_name','gender_age','grade'], 
+  value_vars = ['fractions', 'probability'], 
+  value_name = 'score', 
+  var_name = 'exam'
+)
+
+print(students.head())
+print(students.columns)
+print(students.exam.value_counts())
