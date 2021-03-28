0736f02e51382e22ffdad0d36b50e839702ae923
lendoo <lendoo73@gmail.com>
2021-03-28 11:36:33 +0100

    Create more_string_parsing.py

---
+++ python/How_to_Clean_Data_with_Python/more_string_parsing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from students import students
+
+students.grade = students.grade.replace("(\D+)", "", regex = True)
+
+students.grade = pd.to_numeric(students.grade)
+
+avg_grade = students.grade.mean()
+
+print(avg_grade)
