db14d105f086bccafa18b317aa76f62d30ed6bf7
lendoo <lendoo73@gmail.com>
2021-04-02 17:52:44 +0100

    Create challenge.py

---
+++ python/Learn_Recursion_with_Python/Recursion_Python/Recursion_vs_Iteration_Coding_Throwdown/Binary_tree_depth/challenge.py
@@ -0,0 +1,36 @@
+def depth(tree):
+  # Base case: 
+  if not tree:
+    return 0
+  # Recursive step:
+  left_depth = depth(tree["left_child"])
+  right_depth = depth(tree["right_child"])
+
+  if left_depth > right_depth:
+    return left_depth + 1
+  return right_depth + 1
+
+
+# HELPER FUNCTION TO BUILD TREES
+def build_bst(my_list):
+  if len(my_list) == 0:
+    return None
+
+  mid_idx = len(my_list) // 2
+  mid_val = my_list[mid_idx]
+
+  tree_node = {"data": mid_val}
+  tree_node["left_child"] = build_bst(my_list[ : mid_idx])
+  tree_node["right_child"] = build_bst(my_list[mid_idx + 1 : ])
+
+  return tree_node
+
+# HELPER VARIABLES
+tree_level_1 = build_bst([1])
+tree_level_2 = build_bst([1, 2, 3])
+tree_level_4 = build_bst([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]) 
+
+# test cases
+print(depth(tree_level_1) == 1)
+print(depth(tree_level_2) == 2)
+print(depth(tree_level_4) == 4)
