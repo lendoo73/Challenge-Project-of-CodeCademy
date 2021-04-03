5ac54394a8283c1db1b98e8bb9f6947b6e236094
lendoo <lendoo73@gmail.com>
2021-04-03 15:29:46 +0100

    Create script.py

---
+++ python/Linear_Data_Structures/Nodes/Python/script.py
@@ -0,0 +1,27 @@
+class Node:
+  def __init__(self, value, link_node=None):
+    self.value = value
+    self.link_node = link_node
+    
+  def set_link_node(self, link_node):
+    self.link_node = link_node
+    
+  def get_link_node(self):
+    return self.link_node
+  
+  def get_value(self):
+    return self.value
+
+# Add your code below:
+yacko = Node("likes to yak")
+wacko = Node("has a penchant for hoarding snacks")
+dot = Node("enjoys spending time in movie lots")
+
+yacko.set_link_node(dot)
+dot.set_link_node(wacko)
+
+dots_data = yacko.get_link_node().get_value()
+wackos_data = dot.get_link_node().get_value()
+
+print(dots_data)
+print(wackos_data)
