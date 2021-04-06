77c3a0d97b08ba95d34e73e8787c4677f3fcd14a
lendoo <lendoo73@gmail.com>
2021-04-06 16:22:16 +0100

    Create inner_merge_II.py

---
+++ python/Learn_Data_Analysis_with_Pandas/Multiple_Tables_in_Pandas/inner_merge_II.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+sales = pd.read_csv('sales.csv')
+print(sales)
+targets = pd.read_csv('targets.csv')
+print(targets)
+
+sales_vs_targets = pd.merge(sales, targets)
+print(sales_vs_targets)
+
+crushing_it = sales_vs_targets[sales_vs_targets.revenue > sales_vs_targets.target]
+print(crushing_it)
