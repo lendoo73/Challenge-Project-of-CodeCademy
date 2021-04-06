cd653c6176d1d20eb26924a2be78bdd591355f66
lendoo <lendoo73@gmail.com>
2021-04-06 15:24:22 +0100

    Create script.py

---
+++ python/Learn_Data_Analysis_with_Pandas/Aggregates_in_Pandas/script.py
@@ -0,0 +1,20 @@
+import codecademylib
+import pandas as pd
+
+user_visits = pd.read_csv('page_visits.csv')
+#print(user_visits.head())
+
+click_source = user_visits.groupby("utm_source").id.count().reset_index()
+click_source.rename(columns = {"id": "count"}, inplace = True)
+#print(click_source)
+
+click_source_by_month = user_visits.groupby(["utm_source", "month"]).id.count().reset_index()
+#print(click_source_by_month)
+
+click_source_by_month_pivot = click_source_by_month.pivot(
+  columns = "month",
+  index = "utm_source",
+  values = "id"
+).reset_index()
+print(click_source_by_month_pivot)
+
