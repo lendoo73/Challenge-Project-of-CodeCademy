bbf98b1fbc69b1529b218ac875058b0acfaeec1a
lendoo <lendoo73@gmail.com>
2021-04-08 15:17:52 +0100

    Update finding_duplicates.py

--- python/How_to_Implement_Search_Algorithms_with_Python/Linear_Search/Python/finding_duplicates.py
+++ python/How_to_Implement_Search_Algorithms_with_Python/Linear_Search/Python/finding_duplicates.py
@@ -5,10 +5,11 @@ target_city = "New York City"
 #Linear Search Algorithm
 def linear_search(search_list, target_value):
   matches =  [idx for idx in range(len(search_list)) if search_list[idx] == target_value]
+  
   if len(matches):
     return matches
-  else:
-    raise ValueError(f"{target_value} not in list")
+  
+  raise ValueError(f"{target_value} not in list")
 
 #Function call
 tour_stops = linear_search(tour_locations, target_city)
