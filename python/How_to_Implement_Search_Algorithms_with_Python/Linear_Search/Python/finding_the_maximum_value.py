07d5101d740a304ab1e494a981d208d1e2846b9a
lendoo <lendoo73@gmail.com>
2021-04-08 15:25:18 +0100

    Create finding_the_maximum_value.py

---
+++ python/How_to_Implement_Search_Algorithms_with_Python/Linear_Search/Python/finding_the_maximum_value.py
@@ -0,0 +1,18 @@
+# Search list
+test_scores = [88, 93, 75, 100, 80, 67, 71, 92, 90, 83]
+
+#Linear Search Algorithm
+def linear_search(search_list):
+  maximum_score_index = None
+  for idx in range(len(search_list)):
+    print(search_list[idx])
+    if maximum_score_index is None or search_list[idx] > search_list[maximum_score_index]:
+      maximum_score_index = idx
+  return maximum_score_index
+
+# Function call
+highest_score = linear_search(test_scores)
+
+#Prints out the highest score in the list
+print(highest_score)
+
