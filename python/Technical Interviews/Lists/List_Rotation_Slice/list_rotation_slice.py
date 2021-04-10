adf8fbf71a8a7da5392d5ce6909ccfde9dbcfac5
lendoo <lendoo73@gmail.com>
2021-04-10 11:01:57 +0100

    Create list_rotation_slice.py

---
+++ python/Technical Interviews/Lists/List_Rotation_Slice/list_rotation_slice.py
@@ -0,0 +1,24 @@
+# rotate list
+# no time/space requirements
+# return "rotated" version of input list
+
+def rotate(my_list, num_rotations):
+  # if the rotation is greater than the list length:
+  length = len(my_list)
+  rotation = num_rotations % length
+  
+  if rotation == 0 or rotation == length: return my_list
+
+  # split the list:
+  base_part = my_list[ : length - rotation]
+  rotation_part = my_list[length - rotation : ]
+  return rotation_part + base_part
+
+#### TESTS SHOULD ALL BE TRUE ####
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 1), ['f', 'a', 'b', 'c', 'd', 'e'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 1) == ['f', 'a', 'b', 'c', 'd', 'e']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 2), ['e', 'f', 'a', 'b', 'c', 'd'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 2) == ['e', 'f', 'a', 'b', 'c', 'd']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 3), ['d', 'e', 'f', 'a', 'b', 'c'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 3) == ['d', 'e', 'f', 'a', 'b', 'c']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 4), ['c', 'd', 'e', 'f', 'a', 'b'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 4) == ['c', 'd', 'e', 'f', 'a', 'b']))
