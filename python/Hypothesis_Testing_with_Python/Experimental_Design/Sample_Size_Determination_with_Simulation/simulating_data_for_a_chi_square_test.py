e7f5d49dc8614e6f5fca06a91ba791ff3b16112a
lendoo <lendoo73@gmail.com>
2021-04-18 17:54:21 +0100

    Create simulating_data_for_a_chi_square_test.py

---
+++ python/Hypothesis_Testing_with_Python/Experimental_Design/Sample_Size_Determination_with_Simulation/simulating_data_for_a_chi_square_test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+sample_size = 4
+lift = 0.3
+control_rate = 0.5
+name_rate = (1 + lift) * control_rate
+
+sample_control = np.random.choice(
+  ['yes', 'no'], 
+  size = int(sample_size / 2), 
+  p = [control_rate, 1 - control_rate]
+)
+sample_name = np.random.choice(
+  ['yes', 'no'], 
+  size = int(sample_size / 2), 
+  p = [name_rate, 1 - name_rate]
+)
+
+group = ['control'] * int(sample_size / 2) + ['name'] * int(sample_size / 2)
+outcome = list(sample_control) + list(sample_name)
+sim_data = {"Button": group, "Opened": outcome}
+sim_data = pd.DataFrame(sim_data)
+print(sim_data)
