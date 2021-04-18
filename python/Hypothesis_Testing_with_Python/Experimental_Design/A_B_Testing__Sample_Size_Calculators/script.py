560ca94c887a323889267a0f117851d81c39d087
lendoo <lendoo73@gmail.com>
2021-04-18 10:36:14 +0100

    Create script.py

---
+++ python/Hypothesis_Testing_with_Python/Experimental_Design/A_B_Testing__Sample_Size_Calculators/script.py
@@ -0,0 +1,16 @@
+# calculate baseline:
+baseline = 10
+print("baseline: ", baseline)
+
+# calculate MDE:
+new = 14
+MDE = (new - baseline) / baseline * 100
+print("Minimum Detectable Effect: ", MDE)
+
+# calculate significance threshold:
+sig_threshold = 5
+print("significance threshold: ", sig_threshold)
+
+# calculate total sample size: 
+samp_size= 2060
+print("sample size: ", samp_size)
