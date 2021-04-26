ca99bff27d3294732a262cee03fa0840a4213527
lendoo <lendoo73@gmail.com>
2021-04-26 13:14:47 +0100

    Create blossom_lib.py

---
+++ python/Learn_Complex_Data_Structures/Hash Maps/Blossom/blossom_lib.py
@@ -0,0 +1,16 @@
+flower_definitions = [
+    ['begonia', 'cautiousness'], 
+    ['chrysanthemum', 'cheerfulness'], 
+    ['carnation', 'memories'], 
+    ['daisy', 'innocence'], 
+    ['hyacinth', 'playfulness'], 
+    ['lavender', 'devotion'], 
+    ['magnolia', 'dignity'], 
+    ['morning glory', 'unrequited love'], 
+    ['periwinkle', 'new friendship'], 
+    ['poppy', 'rest'], 
+    ['rose', 'love'], 
+    ['snapdragon', 'grace'], 
+    ['sunflower', 'longevity'], 
+    ['wisteria', 'good luck']
+]
