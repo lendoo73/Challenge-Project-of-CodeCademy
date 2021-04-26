43a23c95cc5a8ca054c35a1242276910dcbb530d
lendoo <lendoo73@gmail.com>
2021-04-26 16:46:31 +0100

    Create script.py

---
+++ python/Learn_Complex_Data_Structures/Trees/Python/script.py
@@ -0,0 +1,23 @@
+class TreeNode:
+  def __init__(self, value):
+    self.value = value # data
+    self.children = [] # references to other nodes
+
+  def add_child(self, child_node):
+    # creates parent-child relationship
+    print("Adding " + child_node.value)
+    self.children.append(child_node) 
+    
+  def remove_child(self, child_node):
+    # removes parent-child relationship
+    print("Removing " + child_node.value + " from " + self.value)
+    self.children = [child for child in self.children 
+                     if child is not child_node]
+
+  def traverse(self):
+    # moves through each node referenced from self downwards
+    nodes_to_visit = [self]
+    while len(nodes_to_visit) > 0:
+      current_node = nodes_to_visit.pop()
+      print(current_node.value)
+      nodes_to_visit += current_node.children
