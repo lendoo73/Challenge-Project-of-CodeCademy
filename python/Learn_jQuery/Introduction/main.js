33cdf5d5c4912d0b9fbd8f9c14ade021094dc2ae
lendoo <lendoo73@gmail.com>
2021-04-27 17:57:53 +0100

    Create main.js

---
+++ python/Learn_jQuery/Introduction/main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  const $menuButton = $('.menu-button');
+  const $navDropdown = $('#nav-dropdown');
+
+  $menuButton.on('click', () => {
+    $navDropdown.show();
+  })
+  
+  $navDropdown.on('mouseleave', () => {
+    $navDropdown.hide();
+  })
+})
