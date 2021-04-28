d45a40d092ea12075b1105ab63720d4e7efabdab
lendoo <lendoo73@gmail.com>
2021-04-28 15:24:12 +0100

    Create main.js

---
+++ python/Learn_jQuery/Effects/main.js
@@ -0,0 +1,40 @@
+$(document).ready(() => {
+  const $allImages = $(".first-image, .fade-image, .slide-image");
+  $('.hide-button').on('click', () => {
+    //$('.first-image').hide();
+    $allImages.hide();
+  });
+  
+  $('.show-button').on('click', () => {
+    $('.first-image').show();
+  });
+  
+  $('.toggle-button').on('click', () => {
+    $('.first-image').toggle();
+  });
+  
+  $('.fade-out-button').on('click', () => {
+    $('.fade-image').fadeOut(500);
+  });
+  
+  $('.fade-in-button').on('click', () => {
+    $('.fade-image').fadeIn(4000);
+  });
+  
+  $('.fade-toggle-button').on('click', () => {
+    $('.fade-image').fadeToggle();
+  });
+  
+  $('.up-button').on('click', () => {
+    $(".slide-image").slideUp(100);
+  });
+  
+  $('.down-button').on('click', () => {
+    $(".slide-image").slideDown("slow");
+  });
+  
+  $('.slide-toggle-button').on('click', () => {
+    $(".slide-image").slideToggle();
+  });
+  
+});
