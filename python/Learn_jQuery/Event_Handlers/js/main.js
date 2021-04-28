3395c459bb515f84341daadfaf7d8c5762980097
lendoo <lendoo73@gmail.com>
2021-04-28 17:15:04 +0100

    Create main.js

---
+++ python/Learn_jQuery/Event_Handlers/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  $('.login-button').on('click', () => {
+    $('.login-form').show();
+  });
+  
+  $('.menu-button').on('mouseenter', () => {
+    $('.nav-menu').show()
+  })
+  
+  $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide();
+  })
+  
+  $('.product-photo').on('mouseenter', event => {
+    $(event.currentTarget).addClass('photo-active')
+  }).on('mouseleave', event => {
+    $(event.currentTarget).removeClass('photo-active')
+  })
+  
+}); 
