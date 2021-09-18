9fa08ac1ea40a495888a975964fa51bc17ccf333
lendoo <lendoo73@gmail.com>
2021-09-18 15:58:27 +0100

    Create models.py

---
+++ python/Build Python Web Apps with Django/4. Data in Django/2. Models and Databases/2. Models and Databases/07. Model Metadata/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Owner(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  phone = models.CharField(max_length=30)
+
+class Patient(models.Model):
+  DOG = "DO"
+  CAT = "CA"
+  BIRD = "BI"
+  REPTILE = "RE"
+  OTHER = "OT"
+  ANIMAL_TYPE_CHOICES = [
+    (DOG, "Dog"),
+    (CAT, "Cat"),
+    (BIRD, "Bird"),
+    (REPTILE, "Reptile"),
+    (OTHER, "Other"),
+  ]
+  animal_type = models.CharField(max_length=2, choices=ANIMAL_TYPE_CHOICES, default=OTHER)
+  breed = models.CharField(max_length=200)
+  pet_name = models.CharField(max_length=200)
+  age = models.IntegerField(default=0)
+  owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+  
+  # Add your code below:
+  # Nested class!!!
+  class Meta:
+    ordering = ["pet_name"]
