143f5cafb44517d1edd33274f21fcc20a85cdc0e
lendoo <lendoo73@gmail.com>
2021-09-18 16:47:50 +0100

    Create models.py

---
+++ python/Build Python Web Apps with Django/4. Data in Django/2. Models and Databases/2. Models and Databases/08. Native Model Methods/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+class Owner(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  phone = models.CharField(max_length=30)
+  # Override your Owner __str__ method below:
+  def __str__(self):
+    return self.first_name + " " + self.last_name
+  
+
+
+class Patient(models.Model):
+  DOG = "DO"
+  CAT = "CA"
+  BIRD = "BI"
+  REPTILE = "RE"
+  OTHER = "OT"
+  ANIMAL_TYPE_CHOICES = [
+    (DOG, "Dog"),
+    (CAT, "Cat"),
+    (BIRD, "Bird"),
+    (REPTILE, "Reptile"),
+    (OTHER, "Other"),
+  ]
+  animal_type = models.CharField(max_length=2, choices=ANIMAL_TYPE_CHOICES, default=OTHER)
+  breed = models.CharField(max_length=200)
+  pet_name = models.CharField(max_length=200)
+  age = models.IntegerField(default=0)
+  owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+  # Override your Patient __str__ method below:
+  def __str__(self):
+    return self.pet_name + ", " + self.animal_type
+
+
+
+  class Meta:
+    ordering = ["pet_name"]
