546795193cfc9f0d84b2bc990b81a1d39a19b2ec
lendoo <lendoo73@gmail.com>
2021-12-28 09:20:14 +0000

    Update PlantRepository.java

--- java/Learn Spring/Spring Data and JPA/2. Custom Queries with JPA/PlantRepository.java
+++ java/Learn Spring/Spring Data and JPA/2. Custom Queries with JPA/PlantRepository.java
@@ -6,4 +6,8 @@ import com.codecademy.plants.entities.Plant;
 
 public interface PlantRepository extends CrudRepository<Plant, Integer> {
   List<Plant> findByHasFruitTrue();
+  List<Plant> findByHasFruitFalse();
+  List<Plant> findByQuantityLessThan(Integer quantity);
+  List<Plant> findByHasFruitTrueAndQuantityLessThan(Integer quantity);
+  List<Plant> findByHasFruitFalseAndQuantityLessThan(Integer quantity);
 }
